c1fae0a645c35f5a25d9deb4fd405254f3017309
Akhil Batra <akhilbatra@berkeley.edu>
2015-10-10 13:35:53 -0700

    sup

--- service/api/bing_query.js
+++ service/api/bing_query.js
@@ -5,7 +5,7 @@ exports.post = function(request, response) {
     //   var tables = request.service.tables;
     //   var push = request.service.push;
 
-    response.send(statusCodes.OK, { message : "hi" });
+    response.send(statusCodes.OK, { message : "hiwhatsup" });
 };
 
 exports.get = function(request, response) {
