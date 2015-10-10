aa23ca4fbeb006d709ef34e57ed345e02c989fa6
Akhil Batra <akhilbatra@berkeley.edu>
2015-10-10 11:34:04 -0700

    test2

--- service/api/choosgo_api.js
+++ service/api/choosgo_api.js
@@ -1,3 +1,4 @@
+var Bing = require('node-bing-api')({ accKey: " FsFdL5st/P9Mbtm5M22Jy6z3wb3AAFr/xjpy431mpxI" });
 
 exports.post = function(request, response) {
     // Use "request.service" to access features of your mobile service, e.g.:
