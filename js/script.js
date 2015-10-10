ab54dc9eb1610c78ee93d2072335728fd341bac5
Kadhir Manickam <kadhirvelm@berkeley.edu>
2015-10-10 15:19:41 -0700

    Completed name changes

--- js/script.js
+++ js/script.js
@@ -6,16 +6,16 @@ angular.module('myApp').directive('actTile', function () {
   return {
     restrict: 'A',
     scope: {
-      actor: '='
+      dataR: '='
     },
     replace: true,
     templateUrl: 'html/data-base-template.html',
     link: function (scope, elem, attrs) {
       scope.hi = function () {
-        alert('Why hello there, I\'m ' + scope.actor.name + '.');
+        alert('Why hello there, I\'m ' + scope.dataR.name + '.');
       };
       elem.find('img').on('click', function () {
-        window.open(scope.actor.image);
+        window.open(scope.dataR.image);
       });
     }
   };
