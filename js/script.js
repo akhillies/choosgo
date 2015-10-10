952c88269768ab9031f3444175c270b5eb9c8275
Kadhir Manickam <kadhirvelm@berkeley.edu>
2015-10-10 14:57:00 -0700

    Completed name changes

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 
 angular.module('myApp', []);
 
-angular.module('myApp').directive('actorTile', function () {
+angular.module('myApp').directive('actorTil', function () {
   return {
     restrict: 'A',
     scope: {
