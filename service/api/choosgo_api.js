d2b92ebedb2bc4ce033f71b980a07a1c1e932ba0
Akhil Batra <akhilbatra@berkeley.edu>
2015-10-10 11:35:09 -0700

    test3

--- service/api/choosgo_api.js
+++ service/api/choosgo_api.js
@@ -5,7 +5,7 @@ exports.post = function(request, response) {
     //   var tables = request.service.tables;
     //   var push = request.service.push;
 
-    response.send(statusCodes.OK, { message : 'Hello World!' });
+    response.send(statusCodes.OK, { message : Bing });
 };
 
 exports.get = function(request, response) {
