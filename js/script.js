7110817a8185e14c3c5277b34f18b148a1433521
Kadhir Manickam <kadhirvelm@berkeley.edu>
2015-10-10 14:35:43 -0700

    Completed name changes

--- js/script.js
+++ js/script.js
@@ -12,10 +12,10 @@ angular.module('myApp').directive('actorTile', function () {
     templateUrl: 'html/actor-tile-template.html',
     link: function (scope, elem, attrs) {
       scope.hi = function () {
-        alert('Why hello there, I\'m ' + scope.actor.name + '.');
+        alert('Why hello there, I\'m ' + scope.dataB.name + '.');
       };
       elem.find('img').on('click', function () {
-        window.open(scope.actor.image);
+        window.open(scope.dataB.image);
       });
     }
   };
@@ -25,18 +25,18 @@ angular.module('myApp').controller('myCtrl', function ($scope) {
   $scope.locations = [
     {
       name: 'Golden Gate Bridge',
-      actor: '1700 Fillmore St, San Francisco, CA 94115',
+      actor: 'Golden Gate Bridge, California',
       image: 'https://upload.wikimedia.org/wikipedia/commons/a/a4/Ggb_by_night.jpg'
     },
     {
-      name: 'Cyclops',
-      actor: 'James Marsden',
-      image: 'http://www.nbc.com/the-tonight-show/content/sites/nbcutsjf/files/styles/bit_stacked_resized/public/images/2014/09/23/james-marsden-.jpg.jpeg?itok=PeCqw-9O'
+      name: 'Dosa!',
+      actor: '1700 Fillmore St, San Francisco, CA 94115',
+      image: 'http://www.sfstation.com/files/2011/06/dosa.jpg'
     },
     {
-      name: 'Professor X',
-      actor: 'Patrick Stewart',
-      image: 'https://pmcdeadline2.files.wordpress.com/2013/01/patrickheadshot.rt__130116211928.jpg'
+      name: 'Golden Gate Park',
+      actor: 'San Francisco, CA',
+      image: 'http://img.timeinc.net/time/photoessays/2008/san_francisco/sf_golden_gate.jpg'
     }
   ];
 });
